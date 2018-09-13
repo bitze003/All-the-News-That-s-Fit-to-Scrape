74ba605a8f594565951c89c9daa7c29ffb427ab4
Kennet Bitzer <bitze003@d.umn.edu>
2018-09-13 09:08:15 -0500

    added models, html, css

--- All-the-News-That-s-Fit-to-Scrape/server.js
+++ All-the-News-That-s-Fit-to-Scrape/server.js
@@ -20,27 +20,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/news-scraper");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news-scraper";
 
-request("https://old.reddit.com/r/webdev/", function(error, response, html) {
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
+request("https://news.google.com/?hl=en-US&gl=US&ceid=US:en", function(error, response, html) {
 
   // Load the HTML into cheerio and save it to a variable
   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
   var $ = cheerio.load(html);
 
   // An empty array to save the data that we'll scrape
-  var results = [];
+  var results = {};
 
   // With cheerio, find each p-tag with the "title" class
   // (i: iterator. element: the current element)
-  $("p.title").each(function(i, element) {
+  $("a.VDXfz").each(function(i, element) {
 
     // Save the text of the element in a "title" variable
-    var title = $(element).text();
+    var link = $(element).attr("href");
 
+    var summary = $(element).parent().text()
     // In the currently selected element, look at its child elements (i.e., its a-tags),
     // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("href");
+    var title = $(element).parent().text();
 
     // Save these results in an object that we'll push into the results array we defined earlier
     results.push({
@@ -49,6 +54,14 @@ request("https://old.reddit.com/r/webdev/", function(error, response, html) {
     });
   });
 
+  $("ariticle.SbNwzf").each(function(i, element) {
+//saves summary from different element tag
+    var summary = $(element).parent().text()
+
+    results.push({
+     summary: summary
+    });
+  });
   // Log the results once you've looped through each of the elements found with cheerio
   console.log(results);
 });
