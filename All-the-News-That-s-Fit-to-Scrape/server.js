96422752edecb34f057fee524a892cc04a625b44
Kennet Bitzer <bitze003@d.umn.edu>
2018-09-13 22:03:14 -0500

    added get routes, and completed npr scrape, with success

--- All-the-News-That-s-Fit-to-Scrape/server.js
+++ All-the-News-That-s-Fit-to-Scrape/server.js
@@ -5,6 +5,7 @@ var cheerio = require("cheerio");
 var request = require("request");
 
 // var db = require("./models");
+var db = require("./models");
 
 var PORT = 3001;
 
@@ -26,42 +27,97 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 
-request("https://news.google.com/?hl=en-US&gl=US&ceid=US:en", function(error, response, html) {
+request("https://www.npr.org/sections/news/archive", function(error, res, html) {
 
   // Load the HTML into cheerio and save it to a variable
   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
   var $ = cheerio.load(html);
 
   // An empty array to save the data that we'll scrape
-  var results = {};
+  var result = {};
 
   // With cheerio, find each p-tag with the "title" class
   // (i: iterator. element: the current element)
-  $("a.VDXfz").each(function(i, element) {
+  $("div.archivelist > article").each(function(i, element) {
 
     // Save the text of the element in a "title" variable
-    var link = $(element).attr("href");
-
-    var summary = $(element).parent().text()
+                                                                      // results.link = $(element).children("a").attr("href");
+    
     // In the currently selected element, look at its child elements (i.e., its a-tags),
     // then save the values for any "href" attributes that the child elements may have
-    var title = $(element).parent().text();
-
+    result.title = $(element).children("div.item-info").children("h2.title").html();
+    //summary of the articles
+    result.summary = $(element).children("div.item-info").children("p.teaser").children("a").text();
     // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
+    db.Article.create(result)
+    .then(function(dbArticle) {
+      // View the added result in the console
+      console.log(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      return res.json(err);
     });
+
   });
 
-  $("ariticle.SbNwzf").each(function(i, element) {
-//saves summary from different element tag
-    var summary = $(element).parent().text()
 
-    results.push({
-     summary: summary
-    });
-  });
+  // res.send("scrape complete")
+
+
   // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
+}); 
+
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
